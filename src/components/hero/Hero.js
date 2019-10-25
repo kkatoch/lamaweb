97d38205602dbf8776f5a2b9e15da1ac4f20cff8
Mahendra Katoch <kmkatoch@gmail.com>
2019-10-25 17:01:39 +0100

    Mobile optimisation

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,35 +2,39 @@ import React from "react";
 import Carousel from "../carousel/CarouselImage";
 import "./Hero.css";
 import Typing from "./Typing";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Hero extends React.Component {
-    render() {
-        return (
-            <div className="hero">
-                <div className="hero-overlay typewriter">
-                    <div className="hero-text">
-                        Lama <i className="heart icon"/>
-                        <Typing text={["Artists", "Corporate environment"]}/>
-                    </div>
-                    <div className="button-hero">
-                        <div className="button-left">
-                            <Link to={`/products`}>
-                                <button className="ui secondary large button">Browse</button>
-                            </Link>
-                            <div className="button-right">
-                                <button className="ui secondary large button">
-                                    Getting Started
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <Carousel className="hero-carousel"/>
+  render() {
+    return (
+      <div className="hero">
+        <div className="hero-overlay typewriter">
+          <div className="hero-text">
+            <span className="hero-highlight">
+              "Where Creativity meets Appreciation"
+            </span>
+              <div className="qoute-text">
+              -Founders, Lama Art Ltd
+              </div>
+          </div>
+          <div className="button-hero">
+            <div className="button-left">
+              <Link to={`/products`}>
+                <button className="ui secondary large button">Browse</button>
+              </Link>
+              <div className="button-right">
+                <button className="ui secondary large button">
+                  Getting Started
+                </button>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+
+        <Carousel className="hero-carousel" />
+      </div>
+    );
+  }
 }
 
 export default Hero;
